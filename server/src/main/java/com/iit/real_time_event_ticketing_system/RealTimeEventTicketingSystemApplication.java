ff2b552f4cf1e0b15f7ee3ccc71bb42482edf15d
hansakarathnayake <hndk2002@gmail.com>
2024-12-04 19:28:58 +0530

    init

---
+++ server/src/main/java/com/iit/real_time_event_ticketing_system/RealTimeEventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iit.real_time_event_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingSystemApplication.class, args);
+	}
+
+}
